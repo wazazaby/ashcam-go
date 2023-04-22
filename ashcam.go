c6c5353deeda3d4c2c025ed8806d247ca3d4a06e
Teddy Sommavilla <teddy@batch.com>
2023-04-22 19:16:25 +0200

    use HTTP client interface instead of concrete type

--- ashcam.go
+++ ashcam.go
@@ -1,19 +1,26 @@
 package ashcam
 
 import (
+	"io"
 	"net/http"
 )
 
+type HTTPClient interface {
+	Do(*http.Request) (*http.Response, error)
+	Get(url string) (*http.Response, error)
+	Post(url, contentType string, body io.Reader) (*http.Response, error)
+}
+
 type ClientOption func(*Client)
 
-func WithHTTPClient(h *http.Client) ClientOption {
+func WithHTTPClient(h HTTPClient) ClientOption {
 	return func(c *Client) {
 		c.httpClient = h
 	}
 }
 
 type Client struct {
-	httpClient *http.Client
+	httpClient HTTPClient
 }
 
 func NewClient(options ...ClientOption) *Client {
